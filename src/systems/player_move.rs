76862323e11976ef5ec9a4b1ca2978390ae1cd9b
brettkolodny <brettkolodny@gmail.com>
2019-03-24 13:46:57 -0400

    Player speed is always constant

--- src/systems/player_move.rs
+++ src/systems/player_move.rs
@@ -19,30 +19,52 @@ impl<'s> System<'s> for PlayerMoveSystem {
       let horizontal_movement = input.axis_value("horizontal");
       let vertical_movement = input.axis_value("vertical");
 
-      let x_change: i32 = {
-        if let Some(amount) = vertical_movement {
-          if amount > 0. {
-            transform.set_rotation_euler(0., 0., 0.);
-          } else if amount < 0. {
-            transform.set_rotation_euler(0., 0., 3.14159);
-          }
-          let player_y = transform.translation().y;
-          let scaled_amount = speed.speed * amount as f32;
-          transform.set_y(player_y + scaled_amount);
+      let mut change_x: f32 = {
+        if let Some(amount) = horizontal_movement {
+          speed.speed * amount as f32
+        } else {
+          0.
         }
-        14
       };
 
-      if let Some(amount) = horizontal_movement {
-        if amount > 0. {
-          transform.set_rotation_euler(0., 0., 1.5708);
-        } else if amount < 0. {
-          transform.set_rotation_euler(0., 0., 4.71239);
+      let mut change_y: f32 = {
+        if let Some(amount) = vertical_movement {
+          speed.speed * amount as f32
+        } else {
+          0.
         }
-        let player_x = transform.translation().x;
-        let scaled_amount = speed.speed * amount as f32;
-        transform.set_x(player_x + scaled_amount);
+      };
+
+      if change_x > 0. {
+        transform.set_rotation_euler(0., 0., 1.5708);
+      } else if change_x < 0. {
+        transform.set_rotation_euler(0., 0., 4.71239);
       }
+
+      if change_y > 0. {
+        transform.set_rotation_euler(0., 0., 0.);
+      } else if change_y < 0. {
+        transform.set_rotation_euler(0., 0., 3.14159);
+      }
+
+      if change_y != 0. && change_x != 0. {
+        let result_vector: f32 = speed.speed;
+        let temp = result_vector / (2.0 as f32).sqrt();
+        let mut new_x = temp;
+        let mut new_y = temp;
+        if change_x < 0. {
+          new_x = new_x * -1.;
+        }
+        if change_y < 0. {
+          new_y = new_y * -1.;
+        }
+
+        change_x = new_x;
+        change_y = new_y;
+      }
+
+      transform.set_x(transform.translation().x + change_x);
+      transform.set_y(transform.translation().y + change_y);
     }
   }
 }
