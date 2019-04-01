5ea41a3b2edcb19aa0090e2d2a91be6f24165248
brettkolodny <brettkolodny@gmail.com>
2019-04-01 16:36:12 -0400

    Broke the SpriteSheet resource out into a resource crate

---
+++ src/resources/mod.rs
@@ -0,0 +1,5 @@
+mod spritesheet;
+
+pub use self::{
+  spritesheet::SpriteSheet,
+};
---
+++ src/resources/spritesheet.rs
@@ -0,0 +1,11 @@
+use amethyst::renderer::{SpriteSheetHandle};
+
+pub struct SpriteSheet {
+  pub sprite_sheet: Option<SpriteSheetHandle>,
+}
+
+impl Default for SpriteSheet {
+  fn default() -> Self {
+    SpriteSheet { sprite_sheet: None }
+  }
+}
