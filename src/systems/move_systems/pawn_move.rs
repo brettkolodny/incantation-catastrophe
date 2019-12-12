809ae2c54853df3e84857044ec3defe89b54718e
Brett Kolodny <brettkolodny@gmail.com>
2019-12-11 19:24:36 -0500

    Pawn now turns to face the player

--- src/systems/move_systems/pawn_move.rs
+++ src/systems/move_systems/pawn_move.rs
@@ -1,13 +1,15 @@
 use amethyst::core::{math, timing::Time, Transform};
 use amethyst::ecs::{Join, Read, ReadStorage, System, WriteStorage};
+use std::f32::consts::PI;
 
-use crate::components::{Pawn, Speed};
+use crate::components::{CurrentDirection, Pawn, Speed};
 use crate::resources::{CurrentState, PlayerResource};
 
 pub struct PawnMoveSystem;
 
 impl<'s> System<'s> for PawnMoveSystem {
     type SystemData = (
+        WriteStorage<'s, CurrentDirection>,
         Read<'s, PlayerResource>,
         ReadStorage<'s, Pawn>,
         ReadStorage<'s, Speed>,
@@ -16,20 +18,33 @@ impl<'s> System<'s> for PawnMoveSystem {
         Read<'s, CurrentState>,
     );
 
-    fn run(&mut self, (player, pawns, speeds, mut transforms, time, state): Self::SystemData) {
+    fn run(
+        &mut self,
+        (mut directions, player, pawns, speeds, mut transforms, time, state): Self::SystemData,
+    ) {
         if state.is_paused() {
             return;
         }
 
         if let Some(player) = player.player {
             let player_transform = transforms.get(player).unwrap().clone();
-            for (pawn_transform, pawn_speed, _) in (&mut transforms, &speeds, &pawns).join() {
+            for (pawn_transform, pawn_speed, direction, _) in (&mut transforms, &speeds, &mut directions, &pawns).join() {
                 let player_vector = player_transform.translation();
                 let pawn_vector = pawn_transform.translation();
 
                 let new_vector = player_vector - pawn_vector;
                 let new_vector = math::base::Matrix::normalize(&new_vector);
                 let new_vector = math::Unit::new_unchecked(new_vector);
+
+                if new_vector.x.abs() > new_vector.y.abs() {
+                    if new_vector.x < 0. {
+                        direction.turn_right();
+                        pawn_transform.set_rotation_y_axis(PI);
+                    } else {
+                        direction.turn_left();
+                        pawn_transform.set_rotation_y_axis(0.);
+                    }
+                }
 
                 pawn_transform
                     .prepend_translation_along(new_vector, time.delta_seconds() * pawn_speed.speed);
