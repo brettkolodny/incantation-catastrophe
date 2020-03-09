6886a9d87376473d8d7bc9c822e14d6a3a71c518
Brett Kolodny <brettkolodny@gmail.com>
2020-03-09 17:46:31 -0400

    Updated player_shoot system to be more like other systems

--- src/systems/shoot_systems/player_shoot.rs
+++ src/systems/shoot_systems/player_shoot.rs
@@ -1,6 +1,5 @@
 use amethyst::core::{math::Vector3, timing::Time, Transform};
-use amethyst::ecs::{Entities, Join, System};
-use amethyst::ecs::{Read, WriteStorage};
+use amethyst::ecs::{Read, WriteStorage, Entities, System};
 use amethyst::input::{InputHandler, StringBindings};
 use amethyst::renderer::SpriteRender;
 
@@ -10,7 +9,9 @@ use crate::components::{
     CurrentDirection, CurrentFrame, Direction, GameplayItem, Player, PlayerProjectile, Projectile,
     Size, Speed,
 };
-use crate::resources::{AnimationSpriteSheets, CurrentState};
+use crate::resources::{AnimationSpriteSheets, CurrentState, PlayerResource};
+
+const PROJECTILE_SCALE: f32 = 1.2;
 
 pub struct PlayerShootSystem {
     pub is_shooting: bool,
@@ -22,17 +23,18 @@ impl<'s> System<'s> for PlayerShootSystem {
         WriteStorage<'s, GameplayItem>,
         WriteStorage<'s, Projectile>,
         WriteStorage<'s, PlayerProjectile>,
-        WriteStorage<'s, Player>,
         WriteStorage<'s, Transform>,
         WriteStorage<'s, Speed>,
         WriteStorage<'s, Size>,
         WriteStorage<'s, CurrentDirection>,
         WriteStorage<'s, SpriteRender>,
         Read<'s, AnimationSpriteSheets>,
+        WriteStorage<'s, Player>,
         Entities<'s>,
         Read<'s, InputHandler<StringBindings>>,
         Read<'s, Time>,
         Read<'s, CurrentState>,
+        Read<'s, PlayerResource>,
     );
 
     fn run(
@@ -42,54 +44,56 @@ impl<'s> System<'s> for PlayerShootSystem {
             mut gameplay_items,
             mut projectiles,
             mut player_projectiles,
-            mut players,
             mut transforms,
             mut speeds,
             mut sizes,
             mut directions,
             mut sprite_renders,
             animation_spritesheets,
+            mut players,
             entities,
             input,
             time,
             state,
+            player_resource,
         ): Self::SystemData,
     ) {
         if !state.is_gameplay() {
             return;
         }
 
+        if player_resource.player.is_none() {
+            return;
+        }
+
+        let player_entity = player_resource.player.unwrap();
+
+        let mut player = players.get_mut(player_entity).unwrap();
+
+
         if let Some(true) = input.action_is_down("shoot") {
-            let mut player_transforms_directions: Vec<(Transform, CurrentDirection)> = Vec::new();
+            if !self.is_shooting {
+                player.time_since_shot = 0.;
+                self.is_shooting = true;
 
-            for (mut player, transform, direction) in
-                (&mut players, &transforms, &directions).join()
-            {
-                if !self.is_shooting {
-                    player_transforms_directions.push((transform.clone(), *direction));
-                    player.time_since_shot = 0.;
-                    self.is_shooting = true;
-                } else {
-                    player.time_since_shot += time.delta_seconds();
-                }
-            }
+                let sprite_render = {
+                    SpriteRender {
+                        sprite_sheet: animation_spritesheets.sprite_sheets["player_projectile"].clone(),
+                        sprite_number: 0,
+                    }
+                };
 
-            let sprite_render = {
-                SpriteRender {
-                    sprite_sheet: animation_spritesheets.sprite_sheets["player_projectile"].clone(),
-                    sprite_number: 0,
-                }
-            };
+                let mut projectile_transform = transforms.get(player_entity).unwrap().clone();
+                let projectile_direction = directions.get(player_entity).unwrap().clone();
 
-            for (mut transform, direction) in player_transforms_directions {
-                transform.set_scale(Vector3::new(1.2, 1.2, 1.));
+                projectile_transform.set_scale(Vector3::new(PROJECTILE_SCALE, PROJECTILE_SCALE, 0.));
 
-                match direction.current_direction {
-                    Direction::Right => transform.set_rotation_euler(0., 0., 0.),
-                    Direction::Left => transform.set_rotation_euler(0., 0., PI),
-                    Direction::Up => transform.set_rotation_euler(0., 0., FRAC_PI_2),
-                    Direction::Down => transform.set_rotation_euler(0., 0., PI + FRAC_PI_2),
-                    _ => &transform,
+                match projectile_direction.current_direction {
+                    Direction::Right => projectile_transform.set_rotation_euler(0., 0., 0.),
+                    Direction::Left => projectile_transform.set_rotation_euler(0., 0., PI),
+                    Direction::Up => projectile_transform.set_rotation_euler(0., 0., FRAC_PI_2),
+                    Direction::Down => projectile_transform.set_rotation_euler(0., 0., PI + FRAC_PI_2),
+                    _ => &projectile_transform,
                 };
 
                 entities
@@ -98,13 +102,17 @@ impl<'s> System<'s> for PlayerShootSystem {
                     .with(PlayerProjectile::default(), &mut player_projectiles)
                     .with(Projectile, &mut projectiles)
                     .with(Size::new(16., 16.), &mut sizes)
-                    .with(transform, &mut transforms)
-                    .with(direction, &mut directions)
+                    .with(projectile_transform, &mut transforms)
+                    .with(projectile_direction, &mut directions)
                     .with(sprite_render.clone(), &mut sprite_renders)
                     .with(Speed::new(750.), &mut speeds)
                     .with(CurrentFrame::new(time.absolute_time_seconds()), &mut frames)
                     .build();
+
+                } else {
+                    player.time_since_shot += time.delta_seconds();
             }
+
         } else {
             self.is_shooting = false;
         }
